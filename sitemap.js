8e75f5f2162a5b4096984a84636f065e8ad41b6e
seggsnation <cooper.beck1@education.nsw.gov.au>
2024-06-20 20:55:32 +1000

    Pls work

--- sitemap.js
+++ sitemap.js
@@ -7,4 +7,15 @@ function toggleList(sublistId) {
 
 document.addEventListener("DOMContentLoaded", function() {
     document.body.classList.add("loaded");
+    document.querySelector('.sitemap').classList.add('loaded');
+
+    document.querySelectorAll('.sitemap > li > a').forEach(function(element) {
+        element.addEventListener('click', function(event) {
+            var sublist = this.nextElementSibling;
+            if (sublist && sublist.tagName === 'UL') {
+                event.preventDefault();
+                sublist.style.display = (sublist.style.display === 'block') ? 'none' : 'block';
+            }
+        });
+    });
 });
