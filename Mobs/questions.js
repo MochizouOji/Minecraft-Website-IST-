812dbea4b5d063583499971d28a12fb05494ff5b
Mochizou <zechariah.guo@education.nsw.gov.au>
2024-07-17 03:28:16 +1000

    a

--- Mobs/questions.js
+++ Mobs/questions.js
@@ -4,7 +4,7 @@ function abc() {
 
     const dropdown = document.getElementById("passivemobs");
     const selectedValue = dropdown.value;
-    console.log(selectedValue);
+    console.log(selectedValue); //learn to use log to work out when things work or not kinda like print with python//
     const audio = new Audio("../Audio/yes.mp3");
 
     if (selectedValue === "Villager") {
@@ -23,6 +23,24 @@ function hideme() {
     question2.classList.add("hidden");
     question3.classList.remove("hidden");
     audio.play();
+}
+
+function bcd() {
+
+    const typed = document.getElementById("filltheblank1input");
+    const selectedValue1 = typed.value;
+    console.log(selectedValue1);
+    const audio = new Audio("../Audio/yes.mp3");
+
+    if (selectedValue1 == "creative", "Creative", "CREATIVE") {
+        question3.classList.add("hidden");
+        question4.classList.remove("hidden");
+        audio.play();
+    }
+    else {
+        window.location.href = "dead.html";
+    }
+
 }
 
 
